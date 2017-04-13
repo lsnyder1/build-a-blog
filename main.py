2214d4f71ac7078470edfa57cd29aafb8728a1f4
Lonny Snyder <Lonnysnyder81@gmail.com>
2017-04-13 03:24:11 -0500

    blog posts showing on front page, buggy, needs index pages still

--- main.py
+++ main.py
@@ -15,6 +15,7 @@
 # limitations under the License.
 #
 import webapp2
+import cgi
 import os
 import jinja2
 from google.appengine.ext import db
@@ -26,7 +27,7 @@ jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
 class Blog(db.Model):
     title=db.StringProperty(required=True)
     created=db.DateTimeProperty(auto_now_add=True)
-    content=db.TextProperty(required=True)                               
+    content=db.TextProperty(required=True)
 
 class Handler(webapp2.RequestHandler):
     def renderError(self, error_code):
@@ -39,8 +40,41 @@ class Handler(webapp2.RequestHandler):
 
 class MainPage(Handler):
     def get(self):
-        self.response.write('Hello world!')
+        blogs= db.GqlQuery("Select * FROM Blog ORDER BY created DESC LIMIT 5")
+        t= jinja_env.get_template("main.html")
+        cont=t.render(blogs=blogs)
+        self.response.write(cont)
+
+
+
+
+
+class NewPost(Handler):
+    def get(self):
+
+        t= jinja_env.get_template("newpost.html")
+        cont=t.render(title=self.request.get("title"),
+                      content=self.request.get("content"),
+                      error=self.request.get("error"))
+        self.response.write(cont)
+
+    def post(self):
+        title=self.request.get("title")
+        content=self.request.get("content")
+
+
+        if title and content:
+            a=Blog(title=title,content=content)
+            a.put()
+
+            self.redirect("/")
+
+        else:
+
+            error="Enter a title and content."
+            self.redirect("/newpost?error="+error)
 
 app = webapp2.WSGIApplication([
-    ('/', MainPage)
+    ('/', MainPage),
+    ('/newpost', NewPost)
 ], debug=True)
